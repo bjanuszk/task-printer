ce9c470ea397e802bb77dd9b9a4a3a244ca7bbfc
BJ87 <januszkiewicz.bartosz@gmail.com>
2018-01-15 21:27:53 +0100

    create rectangle

--- taskPrinter.py
+++ taskPrinter.py
@@ -1,1 +1,20 @@
-print("task printer")
+from PIL import Image
+from PIL import ImageDraw
+
+WHITE_RGB = (255, 255, 255)
+
+HEIGHT = 842
+WIDTH = 595
+
+UNIT_HEIGTH = 140
+UNIT_WIDTH = 297
+
+img = Image.new('RGB', (WIDTH, HEIGHT), color=WHITE_RGB)
+draw = ImageDraw.Draw(img)
+draw.rectangle(((0,0),(UNIT_WIDTH,UNIT_HEIGTH)),outline='black')
+draw.text(((0,0)),'test \n dd',fill='black')
+
+
+img.show()
+# img.save('test.jpg')
+print('success')
