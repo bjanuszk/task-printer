b6a701abc956043d81bd5fdc7439350e009d9cd1
bjanuszkiewicz <bjanuszkiewicz@objectivity.co.uk>
2018-01-19 14:24:52 +0100

    Few tasks done.

--- task.py
+++ task.py
@@ -1,5 +1,7 @@
 from PIL import Image
 from PIL import ImageDraw
+from PIL import ImageFont
+import textwrap
 
 class Task(object):
     def __init__(self, user_story, description, estimate):
@@ -13,15 +15,34 @@ class Task(object):
 WHITE_RGB = (255, 255, 255)
 UNIT_HEIGTH = 140
 UNIT_WIDTH = 297
+MARGIN = 5
+MAX_LINE_LENGTH = 40
 
-def create_task_rectangle():
+FONT_25 = ImageFont.truetype('Roboto-Black.ttf', 25)
+FONT_20 = ImageFont.truetype('Roboto-Black.ttf', 20)
+FONT_18 = ImageFont.truetype('Roboto-Black.ttf', 18)
+
+def create_task_rectangle(task):
     UNIT_HEIGTH = 140
     UNIT_WIDTH = 297
 
     img = Image.new('RGB', (UNIT_WIDTH, UNIT_HEIGTH), color=WHITE_RGB)
     draw = ImageDraw.Draw(img)
-    draw.rectangle(((0, 0), (UNIT_WIDTH-1, UNIT_HEIGTH-1)), outline='red')
-    draw.text(((0, 0)), 'test \n dd', fill='black')
-    img.show()
+    draw.rectangle(((0, 0), (UNIT_WIDTH - 1, UNIT_HEIGTH - 1)), outline='red')
+    draw.text(((MARGIN, 0)), task.user_story, fill='black', font=FONT_25)
+    __draw_description(draw, task.description)
+    draw.text(((MARGIN, 110)), 'Left [h]: ' + task.estimate, fill='black', font=FONT_20)
+    return img
 
-create_task_rectangle()
+def mergeImages(images):
+    result = Image.new("RGB", (594, 840), color=WHITE_RGB)
+    #TODO create images matrix
+    result.paste(images[0],(0,0,UNIT_WIDTH,UNIT_HEIGTH))
+    result.show()
+
+def __draw_description(draw, text):
+    wrapped_description = textwrap.wrap(text, MAX_LINE_LENGTH)
+    for x in range(0, wrapped_description.__len__()):
+        draw.text((MARGIN, (40 + (x * 15))), wrapped_description[x], fill='black', font=FONT_18)
+
+# print(FONT_18.getsize('XX'))
--- taskPrinter.py
+++ taskPrinter.py
@@ -1,23 +1,15 @@
+import task
 from PIL import Image
-from PIL import ImageDraw
-from task import Task
 
-WHITE_RGB = (255, 255, 255)
+task_1 = task.Task('MPI-1717','Create /filters endpoint and update postman collection','6')
+task_2 = task.Task('MPI-1717','Create business logic to handle user request','8')
+task_3 = task.Task('MPI-1717','Performance testing','8')
 
-HEIGHT = 842
-WIDTH = 595
+img_1 = task.create_task_rectangle(task_1)
+img_2 = task.create_task_rectangle(task_2)
+img_3 = task.create_task_rectangle(task_3)
+images = [img_1,img_2,img_3]
 
-UNIT_HEIGTH = 140
-UNIT_WIDTH = 297
+task.mergeImages(images)
 
-img = Image.new('RGB', (WIDTH, HEIGHT), color=WHITE_RGB)
-draw = ImageDraw.Draw(img)
-draw.rectangle(((0,0),(UNIT_WIDTH,UNIT_HEIGTH)),outline='black')
-draw.text(((0,0)),'test \n dd',fill='black')
-
-task = Task('a','b',2)
-print(task)
-
-# img.show()
-# img.save('test.jpg')
 print('success')
